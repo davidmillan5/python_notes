0e221559c492fd6569cfa7b728a77a0b8d2cae9e
Cesar David Millan <cesardavidmillan5@gmail.com>
2025-11-17 06:34:50 -0500

    Updating project structures

---
+++ README.md
@@ -0,0 +1,4 @@
+# Notes
+
+In this repo I'm saving helpful information
+about how to start developing in python programming language.
---
+++ Section_5_Display_and_Structure_Output/test.py
@@ -0,0 +1,9 @@
+products = ['table', 'chair', 'door']
+initial_text = "product"
+for product in products:
+    print(f"{initial_text.title()}: {product}")
+
+
+mylist = ['a', 'b', 'c', 'd']
+for item in mylist:
+    print(len(item))
